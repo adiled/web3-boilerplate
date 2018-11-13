1aa439f83d5ef5a658850ad621b3ecaeed7c0de2
Muhammad Adil Shaikh <m.adilshaikh1@gmail.com>
2018-11-13 22:46:04 +0500

    setup project

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  
+  head() {
+    return {
+      title: 'XMNPay'
+    }
+  },
+
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
